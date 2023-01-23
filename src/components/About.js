775d14d6a7f8ea4dc9a911544e5a72ca7d817ef1
IshKevin <carterk279@gmail.com>
2023-01-23 15:51:53 +0200

    finishing the header party

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const about = () => {
   return (
-    <div className='about'>about</div>
+    <div className='about'>
+        <img src="/src/images/image-interactive.jpg" alt=''></img>
+    </div>
   )
 }
 
