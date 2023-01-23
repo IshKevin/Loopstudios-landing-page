ed1145d22f1cd2ea58326e268ab2905e66e2740e
IshKevin <carterk279@gmail.com>
2023-01-23 18:44:11 +0200

    create body section

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,21 @@ import React from 'react'
 const about = () => {
   return (
     <div className='about'>
-        <img src="image-interactive.jpg" alt=''/>
+        <div>
+        <img src={"image-interactive.jpg"} alt=''/>
+        <div>
+            <h3> THE LEADER IN INTERACTIVE VR</h3>
+            <p>Founded in 2011, loopstudio has been producing
+                world-class virtully rearty projects for some of best companies around the 
+                globe our award winning creations have transformed
+                bussiness through digital experience that bind on their brinde 
+            </p>
         </div>
+        <div>
+           <div></div> 
+        </div>
+        </div>
+    </div>
   )
 }
 
