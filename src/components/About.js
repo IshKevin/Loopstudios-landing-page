4a3c9a838845936435466db7e9747858d7cd031d
IshKevin <carterk279@gmail.com>
2023-01-24 11:41:29 +0200

    fixing bugs

--- src/components/About.js
+++ src/components/About.js
@@ -3,17 +3,21 @@ import React from 'react'
 const about = () => {
   return (
     <div className='about'>
-        <div>
-        <img src={"image-interactive.jpg"} alt=''/>
-        <div>
-            <h3> THE LEADER IN INTERACTIVE VR</h3>
-            <p>Founded in 2011, loopstudio has been producing
-                world-class virtully rearty projects for some of best companies around the 
-                globe our award winning creations have transformed
-                bussiness through digital experience that bind on their brinde 
-            </p>
-        </div>
-        <div>
+      <div className='sect1'>
+  <div className='sect1-a'>
+    <img src={"image-interactive.jpg"} alt=''/>
+  </div>
+  <div className='sect1-b'>
+    <h3> THE LEADER IN <br/>INTERACTIVE VR</h3>
+    <p>Founded in 2011, loopstudio has been producing world-class<br/> 
+    virtully rearty projects for some of best companies around the <br/> 
+    globe our award winning creations have transformed <br/>
+    bussiness through digital experience that bind on their brinde 
+    </p>
+  </div>
+</div>
+
+        <div className='sect2'>
            <h3>our creations </h3>
            <button> see all</button> 
         </div>
@@ -32,7 +36,6 @@ const about = () => {
             </div>
         </div>
         </div>
-    </div>
   )
 }
 
