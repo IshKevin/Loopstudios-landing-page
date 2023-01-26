ab766495584f4c4a58c382abb38df3d9376de5b2
IshKevin <carterk279@gmail.com>
2023-01-26 11:27:08 +0200

    bugs in code

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,22 +1,23 @@
 import React from 'react'
 
-function menu() {
-  return (
+
+function Menu(close,nav) {
+console.log(close,nav);
+    return (
     <div className='humbemenu'>
-        <nav>
-            <div className="logo"><svg width="24" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd"><path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z"/></g></svg>
-            </div>
-            <div className="openMenu"><i className="fa fa-bars"></i></div>
-            <div className='links'>
-            <a href={'#'}>About</a>
-            <a href='#'>Careers</a>
-            <a href='#'>Events</a>
-            <a href='#'>Products</a>
-            <a href='#'>Support</a>
+        <div className="logo">
+        {/* <svg width="24" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd"><path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z"/></g></svg> */}
+        <div className="openMenu"><i onClick={()=>close(false)} className="fa fa-bars"></i></div>
         </div>
-        </nav>
+        <div className='links'>
+        <a href={'#'}>About</a>
+        <a href='#'>Careers</a>
+        <a href='#'>Events</a>
+        <a href='#'>Products</a>
+        <a href='#'>Support</a>
+        </div>
     </div>
   )
 }
 
-export default menu
+export default Menu
