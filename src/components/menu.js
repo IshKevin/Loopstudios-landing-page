2dff66ceab041efa0dca3d509c6b9017a6642f38
IshKevin <carterk279@gmail.com>
2023-01-27 10:53:39 +0200

    fixing some bugs

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,23 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-function Menu(close,nav) {
-console.log(close,nav);
-    return (
-    <div className='humbemenu'>
-        <div className="logo">
-         <svg  onClick={()=>close(false)}width="24" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd"><path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z"/></g></svg> 
-        <div className="openMenu"><i className="fa fa-bars"></i></div>
-        </div>
-        <div className='links'>
-        <a href={'#'}>About</a>
-        <a href='#'>Careers</a>
-        <a href='#'>Events</a>
-        <a href='#'>Products</a>
-        <a href='#'>Support</a>
-        </div>
+  return (
+    <div>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? "Close" : "Open"}
+      </button>
+      {isOpen && (
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default HamburgerMenu;
